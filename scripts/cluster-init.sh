3fd75707220a772df21f69bd4512b195303161ac
Giosuè Sulipano <sulipano.gs@gmail.com>
2023-08-09 21:51:32 +0200

    Use kind directly instead of bash -c

--- scripts/cluster-init.sh
+++ scripts/cluster-init.sh
@@ -45,7 +45,7 @@ fi
 echo -e ${GREEN}
 echo "> Building the cluster"
 echo -e ${NOCOLOR}
-bash -c '/usr/local/bin/kind create cluster --image kindest/node:v1.25.2 --config ${_config}' || exit 1
+/usr/local/bin/kind create cluster --image kindest/node:v1.25.2 --config ${_config} || exit 1
 
 # Retrieve docker container id
 # Docker >= 1.12 - $HOSTNAME seems to be the short container id
