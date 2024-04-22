03053f383721ce1c6e6fe3630e7426ff8bf1568f
Giosuè Sulipano <34242779+imgios@users.noreply.github.com>
2024-04-22 12:39:57 +0200

    parse the kubectl apply exit status

--- scripts/cluster-init.sh
+++ scripts/cluster-init.sh
@@ -106,7 +106,8 @@ if $INGRESS ; then
     echo -e ${GREEN}
     echo "> Deploying the NGINX Ingress Controller"
     echo -e ${NOCOLOR}
-    nginx_deploy=$(kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/main/deploy/static/provider/kind/deploy.yaml)
+    kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/main/deploy/static/provider/kind/deploy.yaml
+    nginx_deploy=$?
     sleep 30s
     if [ $nginx_deploy -ne 0 ] || ! kubectl wait --namespace ingress-nginx --for=condition=ready pod --selector=app.kubernetes.io/component=controller --timeout=300s ; then
         echo "There was an error while deploying the NGINX Ingress Controller."
